98d09fdcd477baf5fac8cdfaa8720cda5f867f84
Hai Duong Huynh <106815704+YamiKaZeee@users.noreply.github.com>
2023-09-08 16:06:15 -0500

    Create lab5.js

---
+++ lab5.js
@@ -0,0 +1,24 @@
+//===========================================1===========================================
+function sum(arr) {
+    const filteredArray = arr.filter(element => element > 20);
+
+    const sum = filteredArray.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+    return sum;
+}
+
+// Example usage:
+const numbers = [10, 25, 5, 30, 15];
+const result = sum(numbers);
+console.log(result); // Output: 55 (25 + 30)
+
+//===========================================2===========================================
+const getNewArray = function (strArray) {
+    const newArray = strArray.filter(str => str.length >= 5 && str.includes('a'));
+
+    return newArray;
+}
+
+const words = ["apple", "banana", "cherry", "grape", "apricot"];
+const filteredWords = getNewArray(words);
+console.log(filteredWords); // Output: ["apple", "apricot"]
